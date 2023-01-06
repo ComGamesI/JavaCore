afa508414b6532ceed3c6070966d82c3ac5e5f5d
legan100 <niconagel@outlook.de>
2023-01-06 10:30:12 +0100

    feat: add FileAPI

---
+++ src/main/java/dev/syster42/fileapi/Main.java
@@ -0,0 +1,36 @@
+package dev.syster42.fileapi;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        FileAPI fileAPI = new FileAPI("test.txt");
+        fileAPI.createFile();
+da
+        System.out.println(fileAPI.canExecute());
+        fileAPI.writeInNextFreeLine("55");
+        System.out.println();
+        System.out.println(fileAPI.canRead());
+        System.out.println(fileAPI.getFile().getAbsoluteFile());
+        System.out.println(fileAPI.canWrite());
+        System.out.println(fileAPI.exists());
+        System.out.println(fileAPI.getFilename());
+        System.out.println(fileAPI.getPath());
+        System.out.println(fileAPI.isDirectory());
+        System.out.println(fileAPI.isFile());
+        System.out.println(fileAPI.isHidden());
+        System.out.println(fileAPI.lastModified());
+        System.out.println(fileAPI.length());
+        System.out.println(fileAPI.readAll());
+        System.out.println(fileAPI.readLine(1));
+        System.out.println(fileAPI.renameFile("log.txt"));
+        scanner.nextLine();
+        fileAPI.writeOverrideAll("d");
+        scanner.nextLine();
+        fileAPI.deleteFile();
+
+
+    }
+}
