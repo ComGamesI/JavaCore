1db8a617cd889f7d027b9008f4ea90daf6b38b9f
legan100 <Niconagel@outlook.de>
2023-01-19 21:13:59 +0100

    feat: add getter ServerAPI

--- src/main/java/dev/syster42/framework/Main.java
+++ src/main/java/dev/syster42/framework/Main.java
@@ -1,17 +1,20 @@
 package dev.syster42.framework;
 
+import dev.syster42.framework.api.ServerAPI;
 import dev.syster42.framework.serverhandler.LogHandler;
 
 public class Main {
     static LogHandler logger = new LogHandler();
+    static ServerAPI serverAPI = new ServerAPI();
 
+    public static ServerAPI getServerAPI() {
+        return serverAPI;
+    }
 
     public static LogHandler getLogger() {
         return logger;
     }
 
-    public void setLogger(LogHandler logger) {
-        this.logger = logger;
-    }
+
 
 }
