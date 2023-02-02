97979f5629492d41bb570b7e7c08fcb56212b30b
legan100 <Niconagel@outlook.de>
2023-02-02 14:19:10 +0100

    fix: Main startuperror

--- src/main/java/dev/syster42/framework/Framework.java
+++ src/main/java/dev/syster42/framework/Framework.java
@@ -3,7 +3,7 @@ package dev.syster42.framework;
 import dev.syster42.framework.api.ServerAPI;
 import dev.syster42.framework.serverhandler.LogHandler;
 
-public class Main {
+public class Framework {
     static LogHandler logger = new LogHandler();
     static ServerAPI serverAPI = new ServerAPI();
 
