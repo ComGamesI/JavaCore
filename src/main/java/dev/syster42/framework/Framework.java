aecb882ccd12603b85b24e0fbbd49ab25ffe9b78
legan100 <niconagel@outlook.de>
2023-06-08 12:47:43 +0200

    reaf: Framework

--- src/main/java/dev/syster42/framework/Framework.java
+++ src/main/java/dev/syster42/framework/Framework.java
@@ -4,12 +4,8 @@ import dev.syster42.framework.api.ServerAPI;
 import dev.syster42.framework.serverhandler.LogHandler;
 
 public class Framework {
-    static LogHandler logger = new LogHandler();
-    static ServerAPI serverAPI = new ServerAPI();
 
-    public static LogHandler getLogger() {
-        return logger;
-    }
+    public static ServerAPI serverAPI = new ServerAPI();
 
     public static ServerAPI getServerAPI() {
         return serverAPI;
